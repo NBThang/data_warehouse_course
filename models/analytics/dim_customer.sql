6fef1a52ca970a6502c13b4185cd2067496e4d35
NBTHang <thangmtp040@gmail.com>
2024-08-25 14:25:00 +0700

    0106a

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,16 @@
+WITH dim_customer__source AS (
+  select *
+  from `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename_column as (
+  select
+    customer_id as customer_key,
+    customer_name as customer_name
+  from dim_customer__source
+)
+
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+  customer_key,
+  customer_name
+FROM dim_customer__rename_column
