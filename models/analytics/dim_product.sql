e46b3e38ca6db7c58dad8b461328d0835f2332a3
NBTHang <thangmtp040@gmail.com>
2024-08-27 14:20:31 +0700

    lesson 0107b

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -7,6 +7,7 @@ WITH dim_product__source AS (
   SELECT 
     stock_item_id AS product_key,
     stock_item_name AS product_name,
+    supplier_id as supplier_key,
     brand AS brand_name
   FROM dim_product__source
 )
@@ -15,12 +16,17 @@ WITH dim_product__source AS (
   SELECT 
     CAST(product_key AS INTEGER) AS product_key,
     CAST(product_name AS STRING) AS product_name,
+    CAST(supplier_key as INTEGER) as supplier_key,
     CAST(brand_name AS STRING) AS brand_name
   FROM dim_product__rename_column
 )
 
 SELECT 
-  product_key,
-  product_name,
-  brand_name
-FROM dim_product__cast_type
+  dim_product.product_key,
+  dim_product.product_name,
+  dim_product.supplier_key,
+  dim_supplier.supplier_name,
+  dim_product.brand_name
+FROM dim_product__cast_type AS dim_product
+LEFT JOIN {{ ref ('dim_supplier') }} as dim_supplier
+ON dim_product.supplier_key = dim_supplier.supplier_key
--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,24 @@
+WITH dim_supplier__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__rename_column as (
+  SELECT 
+  supplier_id AS supplier_key,
+  supplier_name as supplier_name
+  from dim_supplier__source
+  
+)
+
+, dim_supplier__cast_type as (
+  select
+  CAST(supplier_key as INTEGER) as supplier_key,
+  CAST(supplier_name as STRING) as supplier_name
+  from
+  dim_supplier__rename_column
+)
 SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+  supplier_key,
+  supplier_name
+FROM dim_supplier__cast_type
